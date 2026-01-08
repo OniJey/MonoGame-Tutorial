b0b86cb63f5fc7712dca171ab67e6da68ff39c27
OniJey <jeywilkerson@gmail.com>
2026-01-08 13:44:54 -0500

    Added MonoGameLibrary game library as a reusable base for future projects. also added icon to render pipeline to test functionality.

--- Game1.cs
+++ Game1.cs
@@ -1,19 +1,17 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace MonoGame_Tutorial;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
+    private Texture2D _logo;
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        
     }
 
     protected override void Initialize()
@@ -25,9 +23,9 @@ public class Game1 : Game
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        base.LoadContent();
 
-        // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
     }
 
     protected override void Update(GameTime gameTime)
@@ -44,8 +42,18 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
-
+        draw(
+            () => {
+                SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+            }
+        );
         base.Draw(gameTime);
+    }
+
+    private void draw(Action func)
+    {
+        SpriteBatch.Begin();
+        func();
+        SpriteBatch.End();
     }
 }
---
+++ MonoGameLibrary/Core.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameLibrary;
+
+/// <summary>
+/// Allows us to simplify the main class by keeping the more complex systems in this reusable library class
+/// </summary>
+public class Core : Game
+{
+
+    internal static Core s_instance;
+
+    /// <summary>
+    /// gets a reference to core instance
+    /// </summary>
+    public static Core Instance => s_instance;
+
+    ///<summary>
+    ///Gets the graphics device manager to control graphics presentation
+    ///</summary>
+    public GraphicsDeviceManager Graphics {get;private set;}
+
+    /// <summary>
+    /// Gets graphics device used to perform primative rendering tasks
+    /// </summary>
+    public GraphicsDevice _GraphicsDevice {get; private set;}
+    
+
+    /// <summary>
+    /// gets spritebatch used for all 2d rendering
+    /// </summary>
+    public SpriteBatch SpriteBatch {get; private set;}
+
+    /// <summary>
+    /// gets the Content manager used to load assets that are global in scope
+    /// </summary>
+    public static ContentManager Content {get; private set;}
+
+    /// <summary>
+    /// instantiates new core
+    /// </summary>
+    /// <param name = "title"> The title of the game window
+    /// <param name = "width"> The width of the game window
+    /// <param name = "height"> The height of the game window
+    /// <param name = "fullscreen" Whether or not the window is fullscreen
+    public Core(string title, int width, int height, bool fullscreen)
+    {
+        //check if there is already a running instance and close if so
+        if(s_instance != null)
+        {
+            throw new InvalidOperationException("A Core instance is already present");
+        }
+        
+        //set the global instance to this new instance
+        s_instance = this;
+        Graphics = new GraphicsDeviceManager(this);
+
+        //set Default Graphics State
+        Graphics.PreferredBackBufferHeight = height;
+        Graphics.PreferredBackBufferWidth = width;
+        Graphics.IsFullScreen = fullscreen;
+
+        //apply graphics changes
+        Graphics.ApplyChanges();
+
+        //Set window title
+        Window.Title = title;
+
+        //set Content manager to reference of basegame's content manager
+        Content = base.Content;
+
+        Content.RootDirectory = "Content";
+
+        //set mouse invisible by default
+        IsMouseVisible = true;
+
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        _GraphicsDevice = base.GraphicsDevice;
+        SpriteBatch = new SpriteBatch(_GraphicsDevice);
+    }
+}
